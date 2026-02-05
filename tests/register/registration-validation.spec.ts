c5ebce4556c0d87eb809b9340dc8e148ac4125d6
danailzahariev <d.zahariev@icloud.com>
2026-02-05 15:20:18 +0200

    added tests for unhappy paths for register new users

--- tests/register/registration-validation.spec.ts
+++ tests/register/registration-validation.spec.ts
@@ -2,7 +2,7 @@ import {expect, test} from "../fixtures/baseTest";
 import {ErrorMessages} from "../../utils/messages";
 import {createRandomUser} from '../../utils/test-data/data-generator';
 
-test.describe("Registration Form - First Name Boundaries (BVA)", () => {
+test.describe("Registration Form - First Name Invalid Boundaries (BVA)", () => {
 
     const invalidScenarios = [
         {label: 'Empty (0 chars)', firstName: '', expectedError: ErrorMessages.Register.FirstName},
@@ -24,7 +24,7 @@ test.describe("Registration Form - First Name Boundaries (BVA)", () => {
     }
 });
 
-test.describe("Registration Form - Last Name Boundaries (BVA)", () => {
+test.describe("Registration Form - Last Name Invalid Boundaries (BVA)", () => {
     const invalidScenarios = [
         {label: 'Empty (0 chars)', lastName: '', expectedError: ErrorMessages.Register.LastName},
         {label: 'Max + 1 (33 chars)', lastName: 'A'.repeat(33), expectedError: ErrorMessages.Register.LastName},
@@ -42,7 +42,7 @@ test.describe("Registration Form - Last Name Boundaries (BVA)", () => {
     }
 });
 
-test.describe('Field: Email (HTML5 Validation)', () => {
+test.describe('Registration Form - Email (HTML5 Validation)', () => {
     const scenarios = [
         {label: 'Missing @', email: 'dadamail.bg', expectedPart: "include an '@'"},
         {label: 'Missing domain', email: 'dada@', expectedPart: "enter a part following '@'"},
@@ -59,3 +59,53 @@ test.describe('Field: Email (HTML5 Validation)', () => {
         });
     }
 });
+
+test.describe("Registration Form - Telephone Invalid Boundaries (BVA)", () => {
+    const scenarios = [
+        {label: 'Empty (0 chars)', telephone: '', expectedError: ErrorMessages.Register.Telephone},
+        {label: 'Min Boundary (2 chars)', telephone: '12', expectedError: ErrorMessages.Register.Telephone},
+        {label: 'Max + 1 (33 chars)', telephone: '1'.repeat(33), expectedError: ErrorMessages.Register.Telephone}
+    ];
+
+    for (const scenario of scenarios) {
+        test(`Should show error when Telephone is: ${scenario.label}`, async ({registerPage}) => {
+            const invalidUser = createRandomUser({telephone: scenario.telephone});
+
+            await registerPage.navigate();
+            await registerPage.registerUser(invalidUser);
+
+            await expect(registerPage.telephoneError).toBeVisible();
+        });
+    }
+});
+
+test.describe("Registration Form - Password Invalid Boundaries (BVA)", () => {
+    const scenarios = [
+        {label: 'Empty (0 chars)', password: '', expectedError: ErrorMessages.Register.Password},
+        {label: 'Min - 1 (3 chars)', password: '1'.repeat(3), expectedError: ErrorMessages.Register.Password},
+    ];
+
+    for (const scenario of scenarios) {
+        test(`Should show error when Password is: ${scenario.label}`, async ({registerPage}) => {
+            const invalidUser = createRandomUser({password: scenario.password});
+
+            await registerPage.navigate();
+
+            await registerPage.registerUser(invalidUser);
+
+            await expect(registerPage.passwordError).toBeVisible();
+        });
+    }
+});
+
+test.describe("Registration Form - Password don't match", () => {
+    test(`Should show error when Password don't match`, async ({registerPage}) => {
+        const userWithMismatch = createRandomUser({password: "WrongPassword123"})
+
+        await registerPage.navigate();
+        await registerPage.registerUser(userWithMismatch);
+
+        await expect(registerPage.passwordMismatchError).toBeVisible();
+        await expect(registerPage.passwordMismatchError).toHaveText(ErrorMessages.Register.PasswordDontMatch);
+    })
+})
