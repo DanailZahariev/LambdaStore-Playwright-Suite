daa429dc5fc62f46b481a7648e5d079644366656
danailzahariev <d.zahariev@icloud.com>
2026-02-05 15:19:43 +0200

    added tests for happy paths for register new users

--- tests/register/registration.spec.ts
+++ tests/register/registration.spec.ts
@@ -14,7 +14,7 @@ test.describe("Account Registration Tests", () => {
     });
 });
 
-test.describe('Registration Form - Valid Boundaries', () => {
+test.describe('Registration Form - First Name Valid Boundaries', () => {
 
     const validScenarios = [
         {label: 'Min Boundary (1 char)', firstName: 'A'},
@@ -30,8 +30,68 @@ test.describe('Registration Form - Valid Boundaries', () => {
             await registerPage.registerUser(validUser);
 
             await expect(registerPage.successHeader).toBeVisible();
-
             await expect(registerPage.firstNameError).not.toBeVisible();
+        });
+    }
+});
+
+test.describe('Registration Form - Last Name Valid Boundaries', () => {
+    const validScenarios = [
+        {label: 'Min Boundary (1 char)', lastName: 'A'},
+        {label: 'Max Boundary (32 chars)', lastName: 'A'.repeat(32)}
+    ]
+
+    for (const scenario of validScenarios) {
+        test(`Should register successfully with: ${scenario.label}`, async ({registerPage}) => {
+            const validUser = createRandomUser({lastName: scenario.lastName});
+
+            await registerPage.navigate();
+            await registerPage.registerUser(validUser);
+
+            await expect(registerPage.successHeader).toBeVisible();
+            await expect(registerPage.lastNameError).not.toBeVisible();
+        });
+    }
+});
+
+
+test.describe('Registration Form - Telephone Valid Boundaries', () => {
+    const validScenarios = [
+        {label: 'Min Boundary (3 chars)', telephone: '3'.repeat(3)},
+        {label: 'Max Boundary (32 chars)', telephone: '3'.repeat(32)}
+    ]
+
+    for (const scenario of validScenarios) {
+        test(`Should register successfully with: ${scenario.label}`, async ({registerPage}) => {
+            const validUser = createRandomUser({telephone: scenario.telephone});
+
+            await registerPage.navigate();
+            await registerPage.registerUser(validUser);
+
+            await expect(registerPage.successHeader).toBeVisible();
+            await expect(registerPage.telephoneError).not.toBeVisible();
+        });
+    }
+});
+
+test.describe('Registration Form - Password Valid Boundaries', () => {
+    const validScenarios = [
+        {label: 'Min Boundary (4 chars)', password: '1'.repeat(4)},
+        {label: 'Max Boundary (20 chars)', password: '1'.repeat(20)}
+    ]
+    for (const scenario of validScenarios) {
+        test(`Should register successfully with: ${scenario.label}`, async ({registerPage}) => {
+            const validUser = createRandomUser({
+                password: scenario.password,
+                confirmPassword: scenario.password
+            });
+
+            await registerPage.navigate();
+            await registerPage.registerUser(validUser);
+
+            await expect(registerPage.successHeader).toBeVisible();
+            await expect(registerPage.passwordError).not.toBeVisible();
+
         });
     }
 });
